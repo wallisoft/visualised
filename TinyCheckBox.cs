43c6ea56a28a8802619734ef8e29673d6d2040ac
Steve Wallis <wallisoft@gmail.com>
2025-11-12 04:50:46 +0000

    Auto: 2025-11-12 04:50

--- TinyCheckBox.cs
+++ TinyCheckBox.cs
@@ -10,11 +10,15 @@ public class TinyCheckBox : CheckBox
 {
     public TinyCheckBox()
     {
+        MinWidth = 18;
         Height = 18;
         MinHeight = 18;
         FontSize = 11;
         Padding = new Thickness(0);
+        Margin = new Thickness(0);  // Semicolon, not comma
         HorizontalAlignment = HorizontalAlignment.Left;
         VerticalContentAlignment = VerticalAlignment.Center;
+        BorderBrush = new SolidColorBrush(Color.Parse("#66bb6a"));
+        BorderThickness = new Thickness(1);
     }
 }
